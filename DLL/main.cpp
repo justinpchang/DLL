1c79a84e63212da224d7b4958e0ab55b2d0d5d1f
Justin Chang <justin.p.chang@gmail.com>
2016-07-05 23:14:07 -0400

    Initial Commit

---
+++ DLL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DLL
+//
+//  Created by Justin on 7/5/16.
+//  Copyright © 2016 Justin Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
